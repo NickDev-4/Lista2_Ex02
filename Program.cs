0b1c10e6129419cc7c8434b410afa498b55f25ff
NickDev-4 <nickolasfreire.santos@gmail.com>
2025-03-15 15:19:44 -0300

    Exercicio02

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+
+string Frase1, Flase2;
+Console.Write("Digite uma Frase: ");
+
+Frase1 = Console.ReadLine();
+
+Flase2 = Frase1
+
+.Replace("r", "l")
+.Replace("R", "L");
+
+Console.WriteLine();
+Console.WriteLine("Frase do Cebolinha: {0}", Flase2);
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Lista2_Ex02
+Exercício Cebolinha
+
 Solicite que o usuário digite uma frase. Exiba a frase como se ela fosse dita pelo Cebolinha, da Turma da Mônica.
